5b9c2cf092783c4d28d12302ce25cfd0edd1c5b0
marcioszka <monkiewicz.marta@gmail.com>
2023-04-22 16:51:58 +0200

    Add attributes and instance methods to TodoQuarter

--- TodoQuarter.cs
+++ TodoQuarter.cs
@@ -7,7 +7,41 @@ namespace EisenhowerMatrixApp
 
     public class TodoQuarter
     {
+        private List<TodoItem> TodoItems;
 
+        public TodoQuarter() 
+        {
+            TodoItems = new List<TodoItem>();
+        }
+
+        public void AddItem(string title, DateTime deadline) 
+        {
+            TodoItems.Append(new TodoItem(title, deadline));
+        }
+
+        public void RemoveItem(int index) 
+        { 
+            TodoItems.RemoveAt(index);
+        }
+
+        public void ArchiveItems() 
+        {
+            foreach (TodoItem item in TodoItems)
+            {
+                //if item.IsDone == false -> remove item from TodoItems
+            }
+        }
+
+        public List<TodoItem> GetItems()
+        {
+            return TodoItems;
+        }
+
+        public string ToString()
+        {
+            //return formatted list of TodoItems: [ ] day-month task / [x] day-month submit assignment
+            return "";
+        }
     }
 
 }
