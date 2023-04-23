e556c69b531387aedaf4f70306906f2e96f96477
marcioszka <monkiewicz.marta@gmail.com>
2023-04-23 22:04:13 +0200

    Add methods, some to be filled

--- TodoItem.cs
+++ TodoItem.cs
@@ -1,10 +1,42 @@
 using System;
+using System.ComponentModel;
 
 namespace EisenhowerMatrixApp
 {
     public class TodoItem
     {
-        
+        private string Title;
+
+        private DateTime Deadline;
+
+        private bool IsDone;// = false;
+        [DefaultValue(false)]
+
+        public TodoItem(string title, DateTime deadline)
+        {
+            Title = title;
+            Deadline = deadline;
+        }
+
+        public string GetTitle() { return Title; }
+
+        public DateTime GetDeadline() { return Deadline; }
+
+        public void Mark()
+        {
+            IsDone = true;
+        }
+
+        public void Unmark()
+        {
+            IsDone = false;
+        }
+
+        public string ToString(bool IsDone)
+        {
+            //deadline to day-month, IsDone: true -> x, false -> ''
+            return $"[{IsDone}] {GetDeadline().ToString("d-M")} submit assignment";
+        }
     }
 
 }
--- TodoMatrix.cs
+++ TodoMatrix.cs
@@ -4,8 +4,54 @@ namespace EisenhowerMatrixApp
 {
     public class TodoMatrix
     {
+        private Dictionary<string, TodoQuarter> TodoQuarters;
 
+        public TodoMatrix()
+        {
+            TodoQuarters = new Dictionary<string, TodoQuarter>();
+        }
 
+        public Dictionary<string, TodoQuarter> GetQuarters()
+        {
+            return TodoQuarters;
+        }
+
+        public TodoQuarter GetQuarter(string status)
+        {
+            return TodoQuarters[status];
+        }
+
+        public void AddItem(string title, DateTime deadline, bool IsImportant = false)
+        {
+            // Adds new item to dictionary todoQuarters using adequate key.
+            // You should use method AddItem from TodoQuarter class.
+        }
+
+        public void AddItemsFromFile(string filename)
+        {
+            //Reads data from fileName.csv file and appends TodoItem objects to attributes todoItems in the properly TodoQuarter objects.
+            //Every item is written in a separate line in format:
+            //title|day-month|is_important
+        }
+
+        public void SaveItemsToFile(string filenmename)
+        {
+            //Writes all details about TODO items to fileName.csv file
+            //Every item is written in a separate line the following format:
+            //title | day - month | is_important
+        }
+
+        public void ArchiveItems()
+        {
+            //Removes all TodoItem objects with a parameter isDone set to true
+            //from list todoItems in every element of dictionary todoQuarters
+        }
+
+        public string ToString()
+        {
+            //Returns a todoQuarters list (an Eisenhower todoMatrix) formatted to string.
+            return "";
+        }
     }
 
 }
