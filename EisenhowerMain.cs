9834f8dfcf1cf95bc87e48146ab18f6e9bf40c2f
marcioszka <monkiewicz.marta@gmail.com>
2023-05-03 21:22:44 +0200

    Add to avoid merge fail

--- EisenhowerMain.cs
+++ EisenhowerMain.cs
@@ -7,16 +7,7 @@ namespace EisenhowerMatrixApp
     {
         static public void Main(string[] args)
         {
-            //var task1 = new TodoItem("nakarmic rybki", DateTime.Now);
-            //task1.Mark();
-            //var task2 = new TodoItem("podlac kwiatki", DateTime.Now.AddDays(1));
-            //var task3 = new TodoItem("zrobic pranie", DateTime.Now.AddDays(-1));
-            //Console.WriteLine(task1.ToString());
-            //Console.WriteLine(task2.ToString());
-            //Console.WriteLine(task3.ToString());
-            //Console.WriteLine();
-            //Console.WriteLine();
-
+            
             TodoQuarter importantUrgent = new TodoQuarter();
             importantUrgent.AddItem("nakarmic psa", DateTime.Now);
             importantUrgent.AddItem("odkurzyc dom", DateTime.Now.AddDays(1));
@@ -29,19 +20,11 @@ namespace EisenhowerMatrixApp
                 Console.WriteLine($"{item.GetTitle()} {item.GetStatus()}");
             }
             importantUrgent.ArchiveItems();
-            //importantUrgent.AddItem("wyrzucic smieci", DateTime.Now.AddMonths(2));
-            //var tasks = importantUrgent.GetItems();
+            
 
             Console.WriteLine("\nPo archiwizacji:");
             Console.WriteLine(importantUrgent.ToString());
-            //foreach (var task in tasks)
-            //{  }
-            //
-            //todoQuarter.AddItem("odkurzyc salon", DateTime.Now.AddDays(-3));
-            //var taskList = todoQuarter.GetItems();
-
-            //string list = taskList.ToString();
-            //Console.WriteLine(list);
+            
 
             var date = StringHelper.SetDeadlineYear("26", "1");
             Console.WriteLine(date);
