2942acd6ec769326e2b21ec15274609c47d4c403
AlexRogowska <alexrogowska92@gmail.com>
2023-05-15 22:58:39 +0200

    Adding SQL implementation.

---
+++ src/EisenhowerMartixApp/Model/MssqlTodoItemDao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EisenhowerMatrixApp.src.EisenhowerMartixApp.Model
+{
+    internal class MssqlTodoItemDao : ITodoItemDao
+    {
+        public void Add(TodoItem todoItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoItem Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TodoItem todoItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
