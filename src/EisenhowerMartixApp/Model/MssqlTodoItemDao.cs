d7d532e29eb5f6e887a5d557736c749e66594636
marcioszka <monkiewicz.marta@gmail.com>
2023-05-15 23:55:41 +0200

    Install packages, fix bugs

--- src/EisenhowerMartixApp/Model/MssqlTodoItemDao.cs
+++ src/EisenhowerMartixApp/Model/MssqlTodoItemDao.cs
@@ -2,15 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EisenhowerMatrixApp.src.EisenhowerMartixApp.Model
 {
-    internal class MssqlTodoItemDao : ITodoItemDao
+    public class MssqlTodoItemDao : ITodoItemDao
     {
         private readonly string _connectionString;
+
         public MssqlTodoItemDao(string connectionString)
         {
             _connectionString = connectionString;
